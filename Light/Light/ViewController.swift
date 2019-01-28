e704dbbf705ab8d89ced53d3f2a747490943cc74
Tajivon Wells <Tajivon@hotmail.com>
2019-01-27 19:22:41 -0600

    First Update:
    Central Code

--- Light/Light/ViewController.swift
+++ Light/Light/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
+var lightOn = true
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var MyButton: UIButton!
+    
 
     override func viewDidLoad() {
+        MyButton.setTitleColor(.yellow, for: .normal)
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        lightOn = !lightOn
+        updateUI()
+    }
+    
+    func updateUI() {
+        
+        if lightOn {
+            print("Light is on!")
+            view.backgroundColor = .white
+            MyButton.setTitle("Off", for: .normal)
+            MyButton.setTitleColor(.red, for: .normal)
+        } else {
+            print("Light is off!")
+            view.backgroundColor = .black
+            MyButton.setTitle("On", for: .normal)
+            MyButton.setTitleColor(.green, for: .normal)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //Dispose of any resources that can be created
+    }
 
 }
 
